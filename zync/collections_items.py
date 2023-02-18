d5e5d296d61c294ef0d08e733632e520425ba4ab
yun <liyun.elf@gmail.com>
2023-02-18 18:47:07 +0100

    query for item info from db

--- zync/collections_items.py
+++ zync/collections_items.py
@@ -30,7 +30,7 @@ class Collection:
         select itemID from collectionItems
         where collectionID = {self.collection_id}
         """
-        df = query_from_db(qry, ['item_id'])
+        df = query_from_db(qry, ["item_id"])
         self.item_ids = df.item_id.astype(int).tolist()
 
     def update_full_path(self, all_collections: Dict):
@@ -42,3 +42,61 @@ class Collection:
             reverse_list.append(parent.name)
             parent_id = parent.parent_id
         self.full_path = "/".join(reverse_list[::-1])
+
+
+@dataclass
+class Item:
+    item_id: int
+    item_type_id: int  # TODO is this useful?
+    key: str
+    author_last_name: str
+    author_first_name: str
+    title: str = ""
+    publish_year: str = "yyyy"
+    extra_tag: Optional[str] = None
+
+    def __post_init__(self):
+        self.item_id = int(self.item_id)
+        self.item_type_id = int(self.item_type_id)
+        self.author_last_name = (
+            self.author_last_name.strip().replace(" ", "_").replace(".", "_")
+        )
+        self.author_first_name = (
+            self.author_first_name.strip().replace(" ", "_").replace(".", "_")
+        )
+
+        self._update_title()
+        self._update_publish_year()
+        self._update_extra_tag()
+
+    def _qry_field(self, field_name: str):
+        qry = f"""
+        select idv.value
+        from items i
+        join itemTypeFields itf on itf.itemTypeID = i.itemTypeID 
+        join fields f on itf.fieldID = f.fieldID
+        join itemData id on id.fieldID = f.fieldID and id.itemID = i.itemid
+        join itemDataValues idv on idv.valueID = id.valueID 
+        where i.itemID = {self.item_id}
+        and f.fieldName = '{field_name}'
+        """
+        return qry
+
+    def _update_title(self):
+        qry = self._qry_field("title")
+        df = query_from_db(qry, ["title"])
+        self.title = df.title.iloc[0]
+
+    def _update_publish_year(self):
+        qry = self._qry_field("date")
+        df = query_from_db(qry, ["date"])
+        if not df.empty:
+            self.publish_year = str(df.date.iloc[0])[:4]
+
+    def _update_extra_tag(self):
+        qry = self._qry_field("extra")
+        df = query_from_db(qry, ["extra"])
+        if df.empty:
+            return
+        if df.extra.iloc[0] == "TOREAD":  # TODO allow other tags here
+            self.extra_tag = "TOREAD"
